d1388a47976cfe6287f2dab91c6ee7b7ae751b0f
derigel <derigel@259efaa1-37b8-6b43-839f-2492ada40869>
2003-02-13 11:33:16 +0000

    New bindings presenattion.
    git-svn-id: http://localhost:8080/svn/RsdnNntp/trunk@87 259efaa1-37b8-6b43-839f-2492ada40869

---
+++ NNTP/BindingsConverter.cs
@@ -0,0 +1,41 @@
+using System;
+using System.ComponentModel;
+using System.Text;
+
+namespace derIgel.NNTP
+{
+	/// <summary>
+	/// Summary description for BindingsConverter.
+	/// </summary>
+	public class BindingsConverter : ArrayConverter
+	{
+		public BindingsConverter()
+		{
+		}
+
+		public override object ConvertTo(System.ComponentModel.ITypeDescriptorContext context, System.Globalization.CultureInfo culture, object value, System.Type destinationType)
+		{
+			if (destinationType == typeof(string))
+			{
+				StringBuilder list = new StringBuilder();
+				for (int i = 0; i < ((ServerEndPoint[])value).Length; i++)
+				{
+					if (i != 0)
+						list.Append(", ");
+					list.Append(((ServerEndPoint[])value)[i]);
+				}
+				return list.ToString();
+			}
+			else
+				return base.ConvertTo(context, culture, value, destinationType);
+		}
+
+		public override bool CanConvertTo(System.ComponentModel.ITypeDescriptorContext context, System.Type destinationType)
+		{
+			if (destinationType == typeof(string))
+				return true;
+			else
+				return base.CanConvertTo(context, destinationType);
+		}
+	}
+}
