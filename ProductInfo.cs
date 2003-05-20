609606f7b74c23600b99989675dd64032898269c
derigel <derigel@259efaa1-37b8-6b43-839f-2492ada40869>
2003-05-20 21:02:50 +0000

    version 1.5
    git-svn-id: http://localhost:8080/svn/RsdnNntp/trunk@177 259efaa1-37b8-6b43-839f-2492ada40869

--- ProductInfo.cs
+++ ProductInfo.cs
@@ -21,4 +21,4 @@ using System.Runtime.CompilerServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyInformationalVersion("1.5 beta")]
+[assembly: AssemblyInformationalVersion("1.5")]
