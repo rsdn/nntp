3c4e5b69dcc27f86b94baaf2e6c5ff5b9cd0c75f
derigel <derigel@259efaa1-37b8-6b43-839f-2492ada40869>
2003-06-01 20:12:07 +0000

    NEWGROUPS command refactoring.
    git-svn-id: http://localhost:8080/svn/RsdnNntp/trunk@209 259efaa1-37b8-6b43-839f-2492ada40869

---
+++ NNTP/Commands/NewGroups.cs
@@ -0,0 +1,72 @@
+using System;
+using System.Text;
+using System.Text.RegularExpressions;
+
+using Rsdn.Mime;
+
+namespace Rsdn.Nntp.Commands
+{
+	/// <summary>
+	/// NEWGROUPS command.
+	/// </summary>
+	[NntpCommand("NEWGROUPS")]
+	public class NewGroups : Generic
+	{
+		/// <summary>
+		/// Command syntaxis checker.
+		/// </summary>
+		protected static Regex NewGroupsSyntaxisChecker =
+			new	Regex(@"(?in)^NEWGROUPS[ \t]+(?<date>\d{6}[ \t]+\d{6})([ \t]+(?<timezone>GMT))?" +
+								@"([ \t]+<(?<distributions>\w+(.\w+)*(,\w+(.\w+)*)*)>)?[ \t]*$",
+								RegexOptions.Compiled);
+
+		/// <summary>
+		/// Create command handler.
+		/// </summary>
+		/// <param name="session">Parent NNTP session.</param>
+		public NewGroups(Session session) : base(session)
+		{
+			allowedStates = Session.States.Normal;
+			syntaxisChecker = NewGroupsSyntaxisChecker;
+		}
+
+		/// <summary>
+		/// Process command.
+		/// </summary>
+		/// <returns>Server's NNTP Resaponse.</returns>
+		protected override Response ProcessCommand()
+		{
+			try
+			{
+				// get time
+				DateTime date = DateTime.ParseExact(lastMatch.Groups["date"].Value, "yyMMdd HHmmss",
+					null,	System.Globalization.DateTimeStyles.AllowWhiteSpaces);
+				if (lastMatch.Groups["timezone"].Success)
+					// convert GMT to local time
+					date = date.ToLocalTime();
+
+				// get distributions if exist
+				string distributions = null;
+				if (lastMatch.Groups["distributions"].Success)
+				{
+					foreach (string pattern in lastMatch.Groups["distributions"].Value.Split(new char[]{','}))
+						distributions += string.Format("{0}.*|", TransformWildmat(pattern));
+					// remove last |
+					distributions = distributions.Substring(0, distributions.Length - 1);
+				}
+
+				NewsGroup[] groupList = session.DataProvider.GetGroupList(date, distributions);
+				StringBuilder textResponse = new StringBuilder();
+				foreach (NewsGroup group in groupList)
+					textResponse.AppendFormat("{0} {1} {2} {3}",
+						group.Name, group.LastArticleNumber, group.FirstArticleNumber,
+						group.PostingAllowed ? 'y' : 'n').Append(Util.CRLF);
+				return new Response(NntpResponse.ListOfArticles, textResponse.ToString());
+			}
+			catch (ArgumentOutOfRangeException)
+			{
+				return new Response(NntpResponse.SyntaxisError); //wrong date/time
+			}
+		}
+	}
+}
