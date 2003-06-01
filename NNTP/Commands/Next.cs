398ab89ec77f1f69aaef959033b88753dd5623f5
derigel <derigel@259efaa1-37b8-6b43-839f-2492ada40869>
2003-06-01 18:23:25 +0000

    LAST, NEXT commands refactoring.
    git-svn-id: http://localhost:8080/svn/RsdnNntp/trunk@207 259efaa1-37b8-6b43-839f-2492ada40869

---
+++ NNTP/Commands/Next.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Text.RegularExpressions;
+
+namespace Rsdn.Nntp.Commands
+{
+	/// <summary>
+	/// NEXT command.
+	/// </summary>
+	[NntpCommand("NEXT")]
+	public class Next : Generic
+	{
+		/// <summary>
+		/// Command syntaxis checker.
+		/// </summary>
+		protected static Regex NextSyntaxisChecker =
+			new Regex(@"(?in)^NEXT[ \t]*$",	RegexOptions.Compiled);
+
+		/// <summary>
+		/// Create NEXT command handler.
+		/// </summary>
+		/// <param name="session">Parent NNTP session.</param>
+		public Next(Session session) : base(session)
+		{
+			allowedStates = Session.States.Normal;
+			syntaxisChecker = NextSyntaxisChecker;
+		}
+
+		/// <summary>
+		/// Process command.
+		/// </summary>
+		/// <returns>Server's NNTP response</returns>
+		protected override Response ProcessCommand()
+		{
+			if (session.currentGroup == null)
+				throw new DataProviderException(DataProviderErrors.NoSelectedGroup);
+
+			if (session.currentArticle == -1)
+				throw new DataProviderException(DataProviderErrors.NoSelectedArticle);
+
+			NewsArticle article = session.DataProvider.GetNextArticle(session.currentArticle, session.currentGroup);
+
+			session.currentArticle = (int)article.MessageNumbers[session.currentGroup];
+
+			return new Response(NntpResponse.ArticleNothingRetrivied, null,
+				article.MessageNumbers[session.currentGroup], article["Message-ID"]);
+		}
+	}
+}
