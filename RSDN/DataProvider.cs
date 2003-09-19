6567b122cec669f51ebc60f35ef446db7c39cf92
derigel <derigel@259efaa1-37b8-6b43-839f-2492ada40869>
2003-09-19 09:10:13 +0000

    Updating of html template
    git-svn-id: http://localhost:8080/svn/RsdnNntp/trunk@308 259efaa1-37b8-6b43-839f-2492ada40869

--- RSDN/DataProvider.cs
+++ RSDN/DataProvider.cs
@@ -438,7 +438,8 @@ namespace Rsdn.RsdnNntp
 						}
     				string htmlText = string.Format(htmlMessageTemplate, message.authorid, message.author,
     					message.gid, message.id, formatMessage.Format(message.message, true), userType,
-							formatMessage.Format(message.homePage, true), encoding.WebName);
+							formatMessage.Format(message.homePage, true), encoding.WebName,
+							Format.ReplaceTags(message.subject));
     				htmlTextBody.Entities.Add(htmlText);
     				htmlTextBody.TransferEncoding = ContentTransferEncoding.Base64;
     				htmlTextBody.ContentType = string.Format("text/html; charset=\"{0}\"", encoding.WebName);
