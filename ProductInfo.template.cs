ffd6c196cef61595a2c9430ea49d29806b149fff
derigel <derigel@259efaa1-37b8-6b43-839f-2492ada40869>
2005-03-28 10:46:16 +0000

    Use SVN revision for assembly version generation.
    git-svn-id: http://localhost:8080/svn/RsdnNntp/trunk@473 259efaa1-37b8-6b43-839f-2492ada40869

---
+++ ProductInfo.template.cs
@@ -0,0 +1,26 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+//
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+//
+[assembly: AssemblyCompany("Russian Software Developer Network")]
+[assembly: AssemblyProduct("RSDN NNTP Server")]
+[assembly: AssemblyCopyright("© 2002-2005 der Igel, RSDN Group")]
+
+//
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+
+[assembly: AssemblyInformationalVersion("2.0 alpha")]
+
+[assembly: AssemblyVersion("2.0.$WCREV$.*")]
