bc1759efcd192fd7cc0ae61ddd057dde14265317
derigel <derigel@259efaa1-37b8-6b43-839f-2492ada40869>
2005-12-08 13:28:38 +0000

    RSDN NNTP 2.0 Service Release beta started.
    git-svn-id: http://localhost:8080/svn/RsdnNntp/branches/.NET_2.0@535 259efaa1-37b8-6b43-839f-2492ada40869

--- ProductInfo.template.cs
+++ ProductInfo.template.cs
@@ -21,6 +21,6 @@ using System.Runtime.CompilerServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyInformationalVersion("2.0")]
+[assembly: AssemblyInformationalVersion("2.0 SR beta")]
 
 [assembly: AssemblyVersion("2.0.$WCREV$.*")]
