2823c799917ba4440936752cd1a331ab3b16b492
snax <snax@259efaa1-37b8-6b43-839f-2492ada40869>
2007-03-17 08:36:30 +0000

    Updated SvnRevision.exe
    git-svn-id: http://localhost:8080/svn/RsdnNntp/trunk@559 259efaa1-37b8-6b43-839f-2492ada40869

--- Console Test/Class1.cs
+++ Console Test/Class1.cs
@@ -16,26 +16,25 @@ namespace ForumTest
 		/// The main entry point for the application.
 		/// </summary>
 		[STAThread]
-		static void Main(string[] args)
+		static void Main()
 		{
-			ILog logger =
-				log4net.LogManager.GetLogger("RSDN NNTP Server ConsoleApp");
+			ILog logger = LogManager.GetLogger("RSDN NNTP Server ConsoleApp");
 			try
 			{
-        NntpSettings serverSettings =
+				NntpSettings serverSettings =
 					NntpSettings.Deseriazlize(ConfigurationManager.AppSettings["settings.ConfigFile"]);
 
 				Manager nntpManager = new Manager(serverSettings);
 				nntpManager.Start();
 
-				System.Console.ReadLine();
+				Console.ReadLine();
 
 				nntpManager.Stop();
 			}
 			catch (Exception e)
 			{
 				logger.Fatal("ConsoleApp", e);
-				System.Console.ReadLine();
+				Console.ReadLine();
 			}
 		}
 	}
