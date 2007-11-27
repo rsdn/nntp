77adf30f1b1160784973b226cfccad902001b0d3
derigel <derigel@259efaa1-37b8-6b43-839f-2492ada40869>
2007-11-27 16:00:05 +0000

    Run service as console application also
    git-svn-id: http://localhost:8080/svn/RsdnNntp/trunk@577 259efaa1-37b8-6b43-839f-2492ada40869

--- Service/RSDNServer.cs
+++ Service/RSDNServer.cs
@@ -36,17 +36,18 @@ namespace Rsdn.RsdnNntp
 		// The main entry point for the process
 		static void Main()
 		{
-			System.ServiceProcess.ServiceBase[] ServicesToRun;
-
-			// More than one user Service may run within the same process. To add
-			// another service to this process, change the following line to
-			// create a second service object. For example,
-			//
-			//   ServicesToRun = New System.ServiceProcess.ServiceBase[] {new Service1(), new MySecondUserService()};
-			//
-			ServicesToRun = new System.ServiceProcess.ServiceBase[] { new RsdnNntpServer() };
-
-			System.ServiceProcess.ServiceBase.Run(ServicesToRun);
+			// started as service
+			if (Console.In == System.IO.StreamReader.Null)
+			{
+				System.ServiceProcess.ServiceBase.Run(new RsdnNntpServer());
+			}
+			else
+			{
+				RsdnNntpServer server = new RsdnNntpServer();
+				server.OnStart(null);
+				Console.ReadLine();
+				server.OnStop();
+			}
 		}
 
 		/// <summary> 
